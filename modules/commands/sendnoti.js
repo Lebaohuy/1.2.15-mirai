b50a2dd691c0fa9519eaf67c4ae81365ab4dae4a
Lebaohuy <77989902+Lebaohuy@users.noreply.github.com>
2021-07-31 15:25:31 +0700

    Update sendnoti.js

--- modules/commands/sendnoti.js
+++ modules/commands/sendnoti.js
@@ -27,7 +27,7 @@ module.exports.run = async ({ api, event, args, getText }) => {
 	for (const idThread of allThread) {
 		if (isNaN(parseInt(idThread)) || idThread == event.threadID) ""
 		else {
-			api.sendMessage("» THÔNG BÁO TỪ ADMIN ĐẸP TRAI HÀ MẠC TRƯỜNG GIANG «\n\n" + args.join(" ") , idThread, (error, info) => {
+			api.sendMessage("» THÔNG BÁO TỪ ADMIN ĐẸP TRAI SIÊU CẤP VIP PRO «\n\n" + args.join(" ") , idThread, (error, info) => {
 				if (error) cantSend.push(idThread);
 			});
 			count++;
